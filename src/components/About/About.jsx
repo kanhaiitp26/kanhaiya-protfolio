a32da2ec3b5ff71aab8c258849ef215d07ff6351
kanhaiitp26 <lalkanhaiya1931@gmail.com>
2025-05-14 08:29:51 +0530

    resume update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -46,7 +46,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1RGEAzg03GqM073dIxo6R2amD8P457imq/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/17RA4Lf7zwGl98UHi_ZmkUrWorpKPAI51/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
